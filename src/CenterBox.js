ae93e9a433c50210cb0a017a798227101d38aca8
tinxq <tafakurtarig@gmail.com>
2025-09-01 07:44:20 +0300

    1234567

--- src/CenterBox.js
+++ src/CenterBox.js
@@ -33,12 +33,7 @@ function CenterBox() {
         <div className="result">
           <img src={yesImg} alt="Happy celebration" />
           <p>
-            Happy Birthday! I hope your day is full of laughter, fun, joy, and all the things you love. 
-            May this year bring you endless happiness, amazing adventures, great memories, and all the success and love you deserve. 
-            I hope you get to celebrate with the people who make you happiest and that every moment of your day is as special as you are. 
-            You are such an amazing friend and I am so grateful to have you in my life. 
-            Cheers to another year of fun, exciting experiences, growth, and unforgettable moments! 
-            May all your dreams come true and may you always smile and be surrounded by love and positivity.
+            happy birthday
           </p>
           <button className="back" onClick={handleBack}>
             back
